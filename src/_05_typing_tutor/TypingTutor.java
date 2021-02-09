2aa5e397b55396520a35fdfdc774bbc3e8ae8114
Ananya1707 <55816283+Ananya1707@users.noreply.github.com>
2021-02-08 16:57:58 -0800

    Working on Book Of Ilusions

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,14 +1,23 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
 	char currentLetter;
+	JLabel label;
+	JPanel panel;
 	public static void main(String[] args) {
+		TypingTutor tutor = new TypingTutor();
+		tutor.setup(); 
 		
 	}
 	
@@ -19,13 +28,58 @@ public class TypingTutor {
 	
 	void setup() {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+		frame.setVisible(true);
+		panel = new JPanel();
 		
 		currentLetter = generateRandomLetter();
 		
-		JLabel label = new JLabel();
-		label.setText(currentLetter);
-		//
+		label = new JLabel();
+		label.setText(""+currentLetter);
+		
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		
+		frame.addKeyListener(this);
+		frame.add(panel);
+		panel.add(label);
+		
+		frame.pack();
+		
 	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char letter = e.getKeyChar();
+		if(letter == currentLetter) {
+			System.out.println("Correct");
+			panel.setBackground(Color.GREEN);
+		}
+		
+		else{
+			System.out.println("Incorrect");
+			panel.setBackground(Color.RED);
+		}
+		
+		//System.out.println(e.getKeyChar());
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText(""+currentLetter);
+
+		
+		
+	}
+	
 }
